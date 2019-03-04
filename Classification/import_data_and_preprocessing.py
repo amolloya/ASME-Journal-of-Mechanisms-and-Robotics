0b96fbce6078c0d27eb68a228a9925b336405456
Amol Loya <46452977+amolloya@users.noreply.github.com>
2019-03-04 10:22:13 -0500

    Update import_data_and_preprocessing.py

--- Classification/import_data_and_preprocessing.py
+++ Classification/import_data_and_preprocessing.py
@@ -1,29 +1,10 @@
-#import tabula
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import normalize
 from sklearn.decomposition import PCA
-from keras.utils import to_categorical
-import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
-from numpy.random import seed
 
-#seed(1456)
-
-# Read pdf into DataFrame
-#df11 = tabula.read_pdf('Dataset.pdf', nospreadsheet=True, pages = '6', stream=True)
-##df1 = tabula.convert_into("Dataset.pdf", "data.csv", output_format="csv")
-
-#tabula.convert_into("Dataset.pdf", "CA_anthropometric.csv", output_format="csv", pages = '2')
-#tabula.convert_into("Dataset.pdf", "HSP_anthropometric.csv", output_format="csv", pages = '3')
-#tabula.convert_into("Dataset.pdf", "PD_anthropometric.csv", output_format="csv", pages = '4')
-#tabula.convert_into("Dataset.pdf", "HC_anthropometric.csv", output_format="csv", pages = '5')
-
-#tabula.convert_into("Dataset.pdf", "CA.csv", output_format="csv", pages = '6')
-#tabula.convert_into("Dataset.pdf", "HSP.csv", output_format="csv", pages = '7')
-#tabula.convert_into("Dataset.pdf", "PD.csv", output_format="csv", pages = '8')
-#tabula.convert_into("Dataset.pdf", "HC.csv", output_format="csv", pages = '9,10')
-
+# Importing gait parameters of 4 groups
 x1 = pd.read_excel('CA.xlsx')
 x1 = x1.iloc[:,1:]
 
@@ -36,13 +17,15 @@ x3 = x3.iloc[:,1:]
 x4 = pd.read_excel('HC.xlsx')
 x4 = x4.iloc[:,1:]
 
+# Taking care of class imbalance problem by resampling the minimum frequency of classes
 X1 = np.concatenate((x1,x1,x1,x2,x2,x3,x3,x4))
 
-
+# Importing body parameters of 4 groups
 x1 = pd.read_excel('CA_anthropometric.xlsx')
 x1 = x1.drop(columns = ['Subject-ID', 'Disease duration (years)', 'SARA-total', 'Diagnosis'])
 x1 = x1.values
 
+# Assigning 0 and 1 label to Male and Female group respectively
 index = 0
 for ind in range(0,len(x1)):
     if x1[ind,index] == 'M':
@@ -92,63 +75,26 @@ for ind in range(0,len(x4)):
         
 x4 = pd.DataFrame(x4)
 
+# Taking care of class imbalance problem by resampling the minimum frequency of classes
 X2 = np.concatenate((x1,x1,x1,x2,x2,x3,x3,x4))
 X2 = pd.DataFrame(X2, columns=['Gender', 'Age', 'Weight (kg)', 'Height (m)'])
 
+# Concatenating the body and gait parameters into a single array, which acts as an input 
 X = np.concatenate((X2, X1), axis = 1)
 X = pd.DataFrame(X)
 
-#scaler = StandardScaler()
-#X = scaler.fit_transform(X)
-
+# Normalize the input data with L-2 norm
 X = normalize(X, norm='l2', axis = 1)
+
+# Fitting PCA to the input data
 pca = PCA(n_components= 8)
 X = pca.fit_transform(X)
 var = pca.explained_variance_ratio_
 tot_var = sum(var)
 print('Total variance covered by PCs', tot_var)
-print('\n')
 
+# Creating labels for the classes
 Y = np.concatenate((np.zeros(57),np.ones(52),2*np.ones(64),3*np.ones(65)))
-#Y = to_categorical(Y)
+
+# Shuffling the data
 X, Y = shuffle(X, Y, random_state=0)
-
-
-#fig = plt.figure()
-#ax = plt.axes(projection='3d')
-#plt.title('Plot of Top 3 PCs of each disease group',  fontsize=50, ha='center')
-#
-#for i in range(len(X)):
-#    if Y[i] == 0:
-#        l1 = ax.scatter3D(X[i,0], X[i,1], X[i,2], c='r', marker='o', s=40)
-#    if Y[i] == 1:
-#        l2 = ax.scatter3D(X[i,0], X[i,1], X[i,2], c='b', marker='x', s=40)
-#    if Y[i] == 2:
-#        l3 = ax.scatter3D(X[i,0], X[i,1], X[i,2], c='g', marker='*', s=40)
-#    if Y[i] == 3:
-#        l4 = ax.scatter3D(X[i,0], X[i,1], X[i,2], c='m', marker='D', s=40)
-#        
-#ax.set_xlabel('PC1')
-#ax.set_ylabel('PC2')
-#ax.set_xlabel('PC3')     
-#legend = ax.legend((l1, l2, l3, l4), ('CA', 'HSP', 'PD', 'HC'), prop={'size': 18} , borderpad=2)
-#plt.show()
-#
-#
-#fig = plt.figure()
-#plt.title('Plot of Top 2 PCs of each disease group')
-#
-#for i in range(len(X)):
-#    if Y[i] == 0:
-#        plt.plot(X[i,0], X[i,1], marker = 'o', color='r', markersize = 8)
-#    if Y[i] == 1:
-#        plt.plot(X[i,0], X[i,1], marker = 'x', color='b', markersize = 8)
-#    if Y[i] == 2:
-#        plt.plot(X[i,0], X[i,1], marker = '*', color='g', markersize = 8)
-#    if Y[i] == 3:
-#        plt.plot(X[i,0], X[i,1], marker = 'D', color='m', markersize = 8)
-#
-#plt.xlabel('PC1')
-#plt.ylabel('PC2')
-#legend = plt.legend((l1, l2, l3, l4), ('CA', 'HSP', 'PD', 'HC'), prop={'size': 18} , borderpad=2)
-#plt.show()
