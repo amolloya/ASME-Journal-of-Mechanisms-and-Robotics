0c5e51558d28e38f0a0c203079cb733642ebfbde
Amol Loya <46452977+amolloya@users.noreply.github.com>
2019-03-04 11:14:30 -0500

    Update rf_classifier.py

--- Classification/rf_classifier.py
+++ Classification/rf_classifier.py
@@ -1,12 +1,8 @@
+from import_data_and_preprocessing import X,Y
 from sklearn.ensemble import RandomForestClassifier
-from data import X,Y
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.model_selection import LeaveOneOut, KFold
 
-#train_X,valid_X,train_label,valid_label = train_test_split(X, Y, test_size = 0.15)
-
-# Acc of 0.8028 with 7 PCs
+# Cross-validation with Leave one out
 loo = LeaveOneOut()
 acc1 = 0
 square_error_sum = 0.0
@@ -29,13 +25,12 @@ acc1 = acc1/X.shape[0]
 print('Accuracy of the model with LOO:', acc1)
 print('\n')
 
-# Acc = 83.09 for n_est = 110, dep = 11 and 7-fold CV, PCA = 8
+# Cross-validation with k-fold
 for k in (range(2,8)):
     kfold = KFold(k, True, 1)
-    
     acc1 = 0
-    square_error_sum = 0.0
-    count = 0
+    
+    # Cross-validation with Leave one out
     for train_index, test_index in kfold.split(X):
     
         X_train, X_test = X[train_index], X[test_index]
